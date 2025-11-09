434a793c50d4cf14c61b4284686f0ef66f90b031
kimkyoungmin97 <kmboy1210@gmail.com>
2025-11-09 16:20:53 +0900

    프로그래머스 소수찾기 통과

--- src/main/java/pratice/Pratice_DFS_backtracking.java
+++ src/main/java/pratice/Pratice_DFS_backtracking.java
@@ -10,22 +10,17 @@ public class Pratice_DFS_backtracking {
 		
 		String[] arr = {"1","2","3"}; // 배열
 		boolean[] used = new boolean[arr.length]; //사용여부 배열 false로 초기화
-		List<String> list = new ArrayList<>();
-		dfs(arr, used, new StringBuilder(), 0);
-
+		List<String> list = dfs(arr, used, new StringBuilder(), 0, new ArrayList<>());
+		System.out.println(list);
 		
 	}
 	
-	private static void dfs(String[] arr, boolean[] used, StringBuilder sb, int depth) {
+	private static List<String> dfs(String[] arr, boolean[] used, StringBuilder sb, int depth,List<String> list) {
 		
 		//만든 문자열 출력
 		if (depth > 0) {
-	        System.out.println(sb); 
+	        list.add(sb.toString());
 	    }
-		//모든 숫자를 다 쓰면 그냥종료
-		if(depth == arr.length) {
-			return;
-		}
 	
 		for(int i = 0; i < arr.length; ++i) {
 			if(used[i] == true) { //이미 쓴 숫자일 경우 패스
@@ -34,7 +29,7 @@ public class Pratice_DFS_backtracking {
 				used[i] = true; // 숫자 사용
 				sb.append(arr[i]); // 숫자 추가
 				
-				dfs(arr,used,sb,depth+1); // 길이를 1 증가시켜 다시 반복
+				dfs(arr,used,sb,depth+1,list); // 길이를 1 증가시켜 다시 반복
 				
 				//백트래킹
 		        sb.deleteCharAt(sb.length() - 1);
@@ -43,6 +38,7 @@ public class Pratice_DFS_backtracking {
 			
 
 		}
+		return list;
 		
 	}
 
---
+++ src/main/java/programmers/Find_prime_numbers.java
@@ -0,0 +1,72 @@
+package programmers;
+
+import java.util.HashSet;
+import java.util.Set;
+
+//프로그래머스 소수찾기
+
+public class Find_prime_numbers {
+	
+	public static void main(String[] args) {
+        int answer = 0;
+        String numbers = "17";
+        Set<Integer> nums = new HashSet<>();
+        String[] arr = getArr(numbers);
+        boolean[] used = new boolean[arr.length];
+        dfs(arr, used, new StringBuilder(), nums, 0);
+        
+        for(int i : nums) {
+            if(isDecimal(i)){
+                ++answer;
+            }
+        }
+        System.out.println(answer);
+        
+    }
+    
+    private static String[] getArr(String numbers) {
+        String[] arr = new String[numbers.length()];
+        for(int i = 0; i < numbers.length(); ++i){
+            arr[i] = numbers.charAt(i) +"";
+        }
+        return arr;
+    }
+    
+    //소수인지 아닌지 판별
+    // true : 소수 false: 소수아님
+    private static boolean isDecimal(int num) {
+        boolean decimal = true;
+        int count = 0;
+        for(int i = 1; i <= num; ++i){
+            if(num % i == 0){
+                ++count;
+            }
+        }
+        if(count != 2){
+            decimal = false;
+        }
+        return decimal;
+    }
+    //경우의 수 구하기
+    private static void dfs(String[] arr, boolean[] used, StringBuilder path, Set<Integer> nums, int depth) {
+        
+        if(depth > 0) {
+            nums.add(Integer.parseInt(path.toString()));
+        }
+        
+        for(int i = 0; i < arr.length; ++i) {
+            if(used[i] == true) {
+                continue;
+            }else {
+                used[i] = true;
+                path.append(arr[i]);
+                
+                dfs(arr,used,path,nums,depth+1);
+                
+                path.deleteCharAt(path.length()-1);
+                used[i] = false;    
+            }       
+        }
+    }
+
+}
